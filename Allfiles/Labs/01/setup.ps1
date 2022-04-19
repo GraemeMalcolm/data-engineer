5a4486513c3c717fd0b2b17658a85bbc8cf677ef
Graeme Malcolm <gmalc@microsoft.com>
2022-04-19 10:06:01 -0700

    Random delay up to 2 mins

--- Allfiles/Labs/01/setup.ps1
+++ Allfiles/Labs/01/setup.ps1
@@ -45,7 +45,7 @@ $resourceGroupName = "dp000-$suffix"
 
 # Choose a random region
 Write-Host "Finding an available region. This may take several minutes...";
-$delay = 0, 20, 40, 60 | Get-Random
+$delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
 $preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
 $locations = Get-AzLocation | Where-Object {
